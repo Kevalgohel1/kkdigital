6addd8cd99de832d079f3eb02f11add4d4ea049e
keval gohel <digitalkeval1@gmail.com>
2026-04-03 23:55:08 +0530

    Add GA4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -35,6 +36,19 @@ export default function RootLayout({
         />
       </head>
       <body style={{ margin: 0 }}>
+        {/* Google Analytics 4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YY8JNZNP6D"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YY8JNZNP6D');
+          `}
+        </Script>
         {children}
       </body>
     </html>
